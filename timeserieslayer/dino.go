eedcfb6e6f27d44e3184b57a58368ffe8354c274
Keith Kirtfield <keithryankirtfield@gmail.com>
2019-09-19 13:45:07 -0400

    Adding non hard coded web server

--- timeserieslayer/dino.go
+++ timeserieslayer/dino.go
@@ -1,11 +1,20 @@
 package main
 
 import(
-	"fmt"
 	"github.com/keithkfield/Dino/dynowebportal"
+	"os"
+	"log"
+	"encoding/json"
 )
-
+type configuration struct{
+	Webserver string `json:"webserver"`
+}
 func main(){
-	fmt.Println("Hello")
-	dynowebportal.RunWebPortal("localhost:8787")
+	file,err := os.Open("config.json")
+	if err != nil{
+		log.Fatal(err)
+	}
+	config := new(configuration)
+	json.NewDecoder(file).Decode(config)
+	dynowebportal.RunWebPortal(config.Webserver)
 }
