f74eef4a2faf64e3406866acf959eee064c4dfac
Keith Kirtfield <keithryankirtfield@gmail.com>
2019-09-20 11:09:34 -0400

    Connect to mongo

--- timeserieslayer/dino.go
+++ timeserieslayer/dino.go
@@ -4,12 +4,39 @@ import(
 	"github.com/keithkfield/Dino/dynowebportal"
 	"os"
 	"log"
+	"fmt"
+	"context"
 	"encoding/json"
+	// "go.mongodb.org/mongo-driver/bson"
+    "go.mongodb.org/mongo-driver/mongo"
+    "go.mongodb.org/mongo-driver/mongo/options"
 )
 type configuration struct{
 	Webserver string `json:"webserver"`
 }
+type dino struct{
+	name string
+	age string
+}
 func main(){
+	// Set client options
+clientOptions := options.Client().ApplyURI("mongodb+srv://keithkfield:Icecat12!@goconnect-glenv.mongodb.net/test?retryWrites=true&w=majority")
+
+// Connect to MongoDB
+client, err := mongo.Connect(context.TODO(), clientOptions)
+
+if err != nil {
+    log.Fatal(err)
+}
+
+// Check the connection
+err = client.Ping(context.TODO(), nil)
+// collection := client.Database("test").Collection("trainers")
+if err != nil {
+    log.Fatal(err)
+}
+
+fmt.Println("Connected to MongoDB!")
 	file,err := os.Open("config.json")
 	if err != nil{
 		log.Fatal(err)
